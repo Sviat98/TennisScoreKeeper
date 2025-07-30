95d37ac22004e0a4aa0af68cd653b471767b616b
Sviat98 <bashkevich.98@gmail.com>
2025-07-30 19:10:45 +0300

    Fixed CurrentGamePausedComponent.kt

--- composeApp/src/commonMain/kotlin/com/bashkevich/tennisscorekeeper/components/scoreboard/CurrentGamePausedComponent.kt
+++ composeApp/src/commonMain/kotlin/com/bashkevich/tennisscorekeeper/components/scoreboard/CurrentGamePausedComponent.kt
@@ -1,13 +1,18 @@
 package com.bashkevich.tennisscorekeeper.components.scoreboard
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.geometry.toRect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import com.bashkevich.tennisscorekeeper.model.match.domain.TennisGame
 
@@ -16,22 +21,42 @@ fun CurrentGamePausedComponent(
     modifier: Modifier = Modifier,
     currentGame: TennisGame
 ) {
-    Column(
-        modifier = Modifier.then(modifier).padding(2.dp)
-            .border(width = 1.dp, color = Color.White, shape = RoundedCornerShape(4.dp))
-            .padding(horizontal = 4.dp),
-        horizontalAlignment = Alignment.CenterHorizontally, // Выравнивание по центру
+    Box(
+        modifier = Modifier.then(modifier)
     ) {
-        val textColor = Color.White
-        ScoreboardNumber(
-            modifier = Modifier.weight(1f),
-            scoreNumber = currentGame.firstParticipantPointsWon,
-            textColor = textColor
-        )
-        ScoreboardNumber(
-            modifier = Modifier.weight(1f),
-            scoreNumber = currentGame.secondParticipantPointsWon,
-            textColor = textColor
-        )
+        Canvas(modifier = Modifier.matchParentSize().padding(2.dp)) {
+            val path = Path().apply {
+                addRoundRect(
+                    RoundRect(
+                        rect = size.toRect(),
+                        cornerRadius = CornerRadius(4.dp.toPx())
+                    )
+                )
+            }
+            drawPath(
+                path = path,
+                color = Color.White,
+                style = Stroke(width = 1.dp.toPx())
+            )
+        }
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 4.dp).align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally, // Выравнивание по центру
+        ) {
+            val textColor = Color.White
+            ScoreboardNumber(
+                modifier = Modifier.weight(1f),
+                scoreNumber = currentGame.firstParticipantPointsWon,
+                textColor = textColor
+            )
+            ScoreboardNumber(
+                modifier = Modifier.weight(1f),
+                scoreNumber = currentGame.secondParticipantPointsWon,
+                textColor = textColor
+            )
+        }
     }
+
+
 }
